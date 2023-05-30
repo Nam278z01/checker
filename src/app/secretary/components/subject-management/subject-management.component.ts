7947f9c3e57d7754acd8f66d61ebcd1467b578b4
Nam278z01 <nam278z01@gmail.com>
2023-05-30 16:35:08 +0700

    update

--- src/app/secretary/components/subject-management/subject-management.component.ts
+++ src/app/secretary/components/subject-management/subject-management.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-subject-management',
   templateUrl: './subject-management.component.html',
-  styleUrls: ['./subject-management.component.scss']
 })
 export class SubjectManagementComponent {
 
